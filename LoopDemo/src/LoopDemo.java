0850261a86a3944bb1b78cd93496129fd5736d15
Himanshu110 <Himanshugupta11094@gmail.com>
2022-06-30 20:19:48 +0530

    Adding a new one from Eclipse

---
+++ LoopDemo/src/LoopDemo.java
@@ -0,0 +1,24 @@
+
+// for-each Loops
+
+
+public class LoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String arr[]= {"tom", "jerry","man", "JIMMY","KIM"};  ///initialization
+		
+for (String i: arr)
+	
+{
+System.out.println("i val is "+i);	
+}
+ 
+  
+			
+ 
+	}
+
+}
